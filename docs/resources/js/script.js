02d7d2b94e88843d848854ab0c1a74eccde17514
fady tito <fadytito17@gmail.com>
2018-03-30 01:16:26 +0200

    Add some changes

--- docs/resources/js/script.js
+++ docs/resources/js/script.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
 
     /* Navigation scroll (coppied snippt) */
     $(function () {
-        $('a[href*=#]:not([href=#])').click(function () {
+        $('a[href*=\\#]:not([href=\\#])').click(function () {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
